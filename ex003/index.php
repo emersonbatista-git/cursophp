2260c1515328a068834d6639440a5f1d6906a9be
Emerson Santos <ebsantoss@gmail.com>
2025-09-18 10:30:36 -0300

    aula21

---
+++ ex003/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Testes de tipos primitivos</h1>
+    <?php 
+    //0x = hexadecimal  0b = binario 0 = octal
+    // $num = 0x1A;
+
+    // echo "O valor da variavel é $num";
+
+
+    //este comando mostra o tipo de variavel
+    $v = (int) 3e2; //potencia 3x10(2), forçando a ser int
+    var_dump($v);
+
+    ?>
+    
+</body>
+</html>
