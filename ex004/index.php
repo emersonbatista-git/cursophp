f3374ca38d1f5a60407ce07d0d01fc353e4db560
Emerson Santos <ebsantoss@gmail.com>
2025-09-19 11:06:34 -0300

    aula22

---
+++ ex004/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $nome = "Emerson";
+    $sobrenome = "Batista";
+    
+    echo "$nome $sobrenome \u{1F596}";
+
+    echo '<br/>$nome $sobrenome \u{1F596}';
+    
+    ?>
+    
+</body>
+</html>
